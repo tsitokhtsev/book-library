683d7203b29d0637c63e66a9b6e671edf56ad75f
Grigoli Tsitokhtsevi <tsitokhtsev2002@gmail.com>
2024-03-18 22:08:00 +0400

    fix: add auth middleware to admin route

--- routes/admin.php
+++ routes/admin.php
@@ -5,4 +5,4 @@ use Inertia\Inertia;
 
 Route::get('admin/dashboard', function () {
     return Inertia::render('Admin/Dashboard');
-})->middleware(['admin'])->name('admin-dashboard');
+})->middleware(['auth', 'admin'])->name('admin.dashboard');
