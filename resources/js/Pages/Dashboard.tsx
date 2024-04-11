1bd38eb5b4136a3fb93ee384d58037584207f055
Grigoli Tsitokhtsevi <tsitokhtsev2002@gmail.com>
2024-04-11 23:15:07 +0400

    feat: update Dashboard and AdminDashboard layout

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -1,32 +1,18 @@
 import { Head } from '@inertiajs/react';
 import { useLaravelReactI18n } from 'laravel-react-i18n';
 
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { PageProps } from '@/types';
+import MainLayout from '@/Layouts/MainLayout';
 
-export default function Dashboard({ auth }: PageProps) {
+export default function Dashboard() {
     const { t } = useLaravelReactI18n();
 
     return (
-        <AuthenticatedLayout
-            user={auth.user}
-            header={
-                <h2 className="text-xl font-semibold leading-tight text-gray-800">
-                    {t('Dashboard')}
-                </h2>
-            }
-        >
+        <MainLayout>
             <Head title="Dashboard" />
 
-            <div className="py-12">
-                <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                    <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">
-                            {t("You're logged in!")}
-                        </div>
-                    </div>
-                </div>
+            <div className="container">
+                <p>{t("You're logged in!")}</p>
             </div>
-        </AuthenticatedLayout>
+        </MainLayout>
     );
 }
